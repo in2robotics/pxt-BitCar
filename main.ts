2107323df6c8eaea83d1b2c635101d2f27b74c72
MiroChao <531414952@qq.com>
2019-03-22 19:33:28 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,23 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/*****************************************************************************
+* | Description :	BitCar extension for micro:bit
+* | Developer   :   CH Makered
+* | More Info   :	http://chmakered.com/
+******************************************************************************/
+
+/**
+ * Provides access to BitCar blocks for micro: bit functionality.
+ */
+//% color=190 icon="\uf126" block= "BitCar"
+//% groups="['Analog', 'Digital', 'I2C', 'Grove Modules']"
+namespace BitCar {
+    /**
+     * Set the motors' speed of BitCar
+     */
+    //% block="left $leftmotor \\% right $rightmotor \\%"
+    //% leftmotor.shadow="speedPicker"
+    //% rightmotor.shadow="speedPicker"
+    export function move(leftmotor: number, rightmotor: number) {
+
+    }
+
+}
