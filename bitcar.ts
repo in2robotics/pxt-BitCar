422c8a8b1b3eaecc93387f239aad98255475cfe8
Fenyi <fenyi.shi@seeed.cc>
2021-02-20 16:27:32 +0800

    update

--- bitcar.ts
+++ bitcar.ts
@@ -280,6 +280,148 @@ namespace BitCar {
     }
 
 
+
+
+        
+
+let posi_init = 0;
+
+function InitialPosition(): void {
+    posi_init = 1;
+    return;
+}
+
+/**
+ * 
+ */
+//% shim=BitCar::init
+function init(): void {
+    return;
+}
+
+/**
+ * Do something when a key is pressed, released or clicked
+ */
+//% blockId=OnKey
+//% block="on key $key| is $keyEvent"
+//% group="BitPlayer"
+//% weight=20 
+export function OnKey(key: BitPlayerKey, keyEvent: BitPlayerKeyEvent, handler: Action) {
+    if (!posi_init) {
+        InitialPosition();
+    }
+
+    init();
+    control.onEvent(<number>key, <number>keyEvent, handler); // register handler
+}
+
+
+/**
+* Get the key state (pressed or not)
+* @param key the pin that acts as a button
+*/
+//% blockId=KeyPressed
+//% block="BitPlayer: |key $key| is pressed"
+//% group="BitPlayer"
+//% weight=30 
+export function KeyPressed(key: BitPlayerKey): boolean {
+    if (!posi_init) {
+        InitialPosition();
+    }
+
+    const pin = <DigitalPin><number>key;
+    pins.setPull(pin, PinPullMode.PullUp);
+    return pins.digitalReadPin(<DigitalPin><number>key) == 0;
+}
+
+
+
+/**
+* turn on or off the vibration motor
+*/
+//% blockId=SetMotor
+//% block="BitPlayer: |vibration $on|"
+//% on.shadow="toggleOnOff"
+//% on.defl="true"
+//% group="BitPlayer"
+//% weight=10
+export function SetMotor(on: boolean) {
+    if (on) {
+        pins.digitalWritePin(DigitalPin.P8, 1);
+    }else{
+        pins.digitalWritePin(DigitalPin.P8, 0);
+    }
+}
+
+
+const x0 = 500;
+const y0 = 500;
+const d0 = 250;
+
+/**
+* Check if the joystick is being pushed to a specified direction
+* @param position the current position of joystick
+*/
+//% blockId=OnJoystick
+//% block="BitPlayer: |joystick $position|"
+//% position.fieldEditor="gridpicker"
+//% position.fieldOptions.columns=3
+//% group="BitPlayer"
+//% weight = 40
+export function OnJoystick(position: Joystick): boolean {
+    let x = pins.analogReadPin(AnalogPin.P1) - x0;
+    let y = pins.analogReadPin(AnalogPin.P2) - y0;
+    let d = Math.round(Math.sqrt(Math.abs(x * x) + Math.abs(y * y)));
+    const value1 = Math.round(d * 0.38);        //0.38 is the value of sin 22.5°
+    const value2 = Math.round(d * 0.92);        //0.92 is the value of sin 67.5°
+    let getPosition = Joystick.Middle;
+
+    if (d > d0) {
+        if (x > 0 && y > 0) {               // (x,y) is at top right area
+            if (y > value2) {
+                getPosition = Joystick.Up;
+            } else if (y < value1) {
+                getPosition = Joystick.Right;
+            } else {
+                getPosition = Joystick.UpRight;
+            }
+        } else if (x > 0 && y < 0) {        // (x,y) is at bot right area
+            if (x > value2) {
+                getPosition = Joystick.Right;
+            } else if (x < value1) {
+                getPosition = Joystick.Down;
+            } else {
+                getPosition = Joystick.LowerRight;
+            }
+        } else if (x < 0 && y < 0) {         // (x,y) is at bot left area
+            y = Math.abs(y);
+            if (y > value2) {
+                getPosition = Joystick.Down;
+            } else if (y < value1) {
+                getPosition = Joystick.Left;
+            } else {
+                getPosition = Joystick.LowerLeft;
+            }
+        } else if (x < 0 && y > 0) {         // (x,y) is at top left area
+            if (y > value2) {
+                getPosition = Joystick.Up;
+            } else if (y < value1) {
+                getPosition = Joystick.Left;
+            } else {
+                getPosition = Joystick.UpLeft;
+            }
+        }
+    } else {
+        getPosition = Joystick.Middle;
+    }
+
+    if (getPosition == position) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
     
  
 
@@ -293,149 +435,3 @@ namespace BitCar {
 
 
 
-
-/**
- * Provides access to BitPlayer blocks for micro: bit functionality.
- */
-//% color=190 icon="\uf126" block= "BitPlayer"
-//% groups="['BitPlayer']"
-namespace BitPlayer {
-let posi_init = 0;
-
-    function InitialPosition(): void {
-        posi_init = 1;
-        return;
-    }
-
-	/**
-	 * 
-	 */
-    //% shim=bitplayer::init
-    function init(): void {
-        return;
-    }
-
-    /**
-     * Do something when a key is pressed, released or clicked
-     */
-    //% blockId=OnKey
-    //% block="on key $key| is $keyEvent"
-    //% group="BitPlayer"
-    //% weight=20 
-    export function OnKey(key: BitPlayerKey, keyEvent: BitPlayerKeyEvent, handler: Action) {
-        if (!posi_init) {
-            InitialPosition();
-        }
-
-        init();
-        control.onEvent(<number>key, <number>keyEvent, handler); // register handler
-    }
-
-
-    /**
-    * Get the key state (pressed or not)
-    * @param key the pin that acts as a button
-    */
-    //% blockId=KeyPressed
-    //% block="BitPlayer: |key $key| is pressed"
-    //% group="BitPlayer"
-    //% weight=30 
-    export function KeyPressed(key: BitPlayerKey): boolean {
-        if (!posi_init) {
-            InitialPosition();
-        }
-
-        const pin = <DigitalPin><number>key;
-        pins.setPull(pin, PinPullMode.PullUp);
-        return pins.digitalReadPin(<DigitalPin><number>key) == 0;
-    }
-
-
-
-    /**
-    * turn on or off the vibration motor
-    */
-    //% blockId=SetMotor
-    //% block="BitPlayer: |vibration $on|"
-    //% on.shadow="toggleOnOff"
-    //% on.defl="true"
-    //% group="BitPlayer"
-    //% weight=10
-    export function SetMotor(on: boolean) {
-        if (on) {
-            pins.digitalWritePin(DigitalPin.P8, 1);
-        }else{
-            pins.digitalWritePin(DigitalPin.P8, 0);
-        }
-    }
-
-
-    const x0 = 500;
-    const y0 = 500;
-    const d0 = 250;
-
-    /**
-    * Check if the joystick is being pushed to a specified direction
-    * @param position the current position of joystick
-    */
-    //% blockId=OnJoystick
-    //% block="BitPlayer: |joystick $position|"
-    //% position.fieldEditor="gridpicker"
-    //% position.fieldOptions.columns=3
-    //% group="BitPlayer"
-    //% weight = 40
-    export function OnJoystick(position: Joystick): boolean {
-        let x = pins.analogReadPin(AnalogPin.P1) - x0;
-        let y = pins.analogReadPin(AnalogPin.P2) - y0;
-        let d = Math.round(Math.sqrt(Math.abs(x * x) + Math.abs(y * y)));
-        const value1 = Math.round(d * 0.38);        //0.38 is the value of sin 22.5°
-        const value2 = Math.round(d * 0.92);        //0.92 is the value of sin 67.5°
-        let getPosition = Joystick.Middle;
-
-        if (d > d0) {
-            if (x > 0 && y > 0) {               // (x,y) is at top right area
-                if (y > value2) {
-                    getPosition = Joystick.Up;
-                } else if (y < value1) {
-                    getPosition = Joystick.Right;
-                } else {
-                    getPosition = Joystick.UpRight;
-                }
-            } else if (x > 0 && y < 0) {        // (x,y) is at bot right area
-                if (x > value2) {
-                    getPosition = Joystick.Right;
-                } else if (x < value1) {
-                    getPosition = Joystick.Down;
-                } else {
-                    getPosition = Joystick.LowerRight;
-                }
-            } else if (x < 0 && y < 0) {         // (x,y) is at bot left area
-                y = Math.abs(y);
-                if (y > value2) {
-                    getPosition = Joystick.Down;
-                } else if (y < value1) {
-                    getPosition = Joystick.Left;
-                } else {
-                    getPosition = Joystick.LowerLeft;
-                }
-            } else if (x < 0 && y > 0) {         // (x,y) is at top left area
-                if (y > value2) {
-                    getPosition = Joystick.Up;
-                } else if (y < value1) {
-                    getPosition = Joystick.Left;
-                } else {
-                    getPosition = Joystick.UpLeft;
-                }
-            }
-        } else {
-            getPosition = Joystick.Middle;
-        }
-
-        if (getPosition == position) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-}
