89997eee5920f0f215c3a748ae50f9b9ea866c37
Fenyi <fenyi.shi@seeed.cc>
2021-02-20 17:34:56 +0800

    dd

--- bitcar.ts
+++ bitcar.ts
@@ -296,7 +296,7 @@ function InitialPosition(): void {
  */
 //% shim=BitCar::init
 function init(): void {
-    return;
+    ;
 }
 
 /**
@@ -311,7 +311,7 @@ export function OnKey(key: BitPlayerKey, keyEvent: BitPlayerKeyEvent, handler: A
         InitialPosition();
     }
 
-
+    init();
     control.onEvent(<number>key, <number>keyEvent, handler); // register handler
 }
 
